57ac93dc71688ec876ef56a7ee923a0be10a2352
Vortechron <amirul@baituljannah.com>
2026-04-27 00:02:23 +0800

    fix: restore safer Proton follow gaps

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -72,9 +72,9 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.40
   elif personality==log.LongitudinalPersonality.standard:
-    return 0.8
+    return 1.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.52
+    return 0.6
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
